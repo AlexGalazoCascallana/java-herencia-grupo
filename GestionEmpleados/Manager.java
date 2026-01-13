9c04ed7f03168e146591c34af07cfb393d203519
Alex <159572655+AlexGalazoCascallana@users.noreply.github.com>
2026-01-13 13:52:08 +0000

    clases empleado y Manager

---
+++ GestionEmpleados/Manager.java
@@ -0,0 +1,39 @@
+// Manager.java
+public class Manager extends Empleado {
+    protected int personasACargo;
+    
+
+    public Manager(String dni, String nombre, int edad, 
+                  double salarioBase, int antiguedadAnios, 
+                  int personasACargo) {
+        super(dni, nombre, edad, salarioBase, antiguedadAnios);
+        this.personasACargo = personasACargo;
+    }
+    
+    public int getPersonasACargo() {
+        return personasACargo;
+    }
+    
+    public void setPersonasACargo(int personasACargo) {
+        this.personasACargo = personasACargo;
+    }
+    
+    @Override
+    public double calcularSalario() {
+        // Los managers tienen un bono por personas a cargo (5% por persona)
+        double salarioEmpleado = super.calcularSalario(); // Llama al método de Empleado
+        double bonoPorPersonas = salarioBase * 0.05 * personasACargo;
+        return salarioEmpleado + bonoPorPersonas;
+    }
+    
+    @Override
+    public String toString() {
+        return "Manager: " + nombre + 
+               "\nDNI: " + getDni() + 
+               "\nEdad: " + edad + 
+               "\nPersonas a cargo: " + personasACargo +
+               "\nAntigüedad: " + antiguedadAnios + " años" +
+               "\nSalario base: " + salarioBase + "€" +
+               "\nSalario total: " + String.format("%.2f", calcularSalario()) + "€";
+    }
+}
